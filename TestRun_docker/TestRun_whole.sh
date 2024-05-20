679e466f0ecff4d4feeb028045c0d24249c5fc38
ngs1810 <48265004+ngs1810@users.noreply.github.com>
2024-05-20 18:55:42 +0930

    Update TestRun_whole.sh
    I have tested it on SpongeBob, and it works!  The script has combined necessary MF commands and scripts together.

--- TestRun_docker/TestRun_whole.sh
+++ TestRun_docker/TestRun_whole.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Define variables
-#git_dir="/home/neuro/Documents/Nandini/MosaiC-All"
-git_dir=/Users/nandinisandran/Desktop/MosaiC-All/TestRun_docker
+git_dir="/home/neuro/Documents/Nandini/MosaiC-All"
+#git_dir=/Users/nandinisandran/Desktop/MosaiC-All/TestRun_docker #DELETE
 
 # Other variables should not be changed
 testrun_dir="$git_dir/TestRun_docker"
@@ -11,7 +11,7 @@ proband_bam_file="$bam_dir/1465_1024-pfc-bulk.rehead.bam"
 proband_id="1465_1024-pfc-bulk"
 gender="M"
 config="hg19"
-outdir="$testrun_dir/OUTPUT" 
+outdir="$testrun_dir/OUTPUT"
 resources="$testrun_dir/Resources"
 
 if [ ! -d "${outdir}" ]; then
@@ -27,8 +27,8 @@ wget "https://storage.googleapis.com/genomics-public-data/references/hs37d5/hs37
 
 if [ -e "$resources/hs37d5.fa.gz" ]; then
     # Extract the tar.gz file
-    gunzip "$resources/hs37d5.fa.gz" -c "resources"
-    samtools faidx "hs37d5.fa.gz"
+    gunzip "$resources/hs37d5.fa.gz"
+    samtools faidx "$resources/hs37d5.fa" -o "$resources/hs37d5.fa.fai"
 fi
 
 
@@ -41,7 +41,7 @@ wget "https://storage.googleapis.com/gcp-public-data--broad-references/hg19/v0/d
 # Check if dbsnp_138.b37.vcf.gz  was downloaded successfully
 if [ -e "$resources/dbsnp_138.b37.vcf.gz" ]; then
     # Extract the tar.gz file
-    gunzip "$resources/dbsnp_138.b37.vcf.gz" -c "resources"
+    gunzip "$resources/dbsnp_138.b37.vcf.gz"
 fi
 
 dbsnp="dbsnp_138.b37.vcf"
@@ -55,13 +55,12 @@ docker image pull rborgesm/mosaichunter:1.0
 docker run -it --rm \
   -v "$git_dir:/mnt/mosaic-all" \
   -v "$testrun_dir:/mnt/testrun_docker" \
-  -v "$refgen:/mnt/refgen" \
   rborgesm/mosaichunter:1.0 \
   /bin/bash -c "
   MHDIR=/usr/MosaicHunter
   java -Xmx4G -jar \$MHDIR/build/mosaichunter.jar \
     -C \$MHDIR/conf/exome_parameters.properties \
-    -P reference_file=/mnt/refgen/hs37d5.fa \
+    -P reference_file=/mnt/testrun_docker/Resources/hs37d5.fa \
     -P input_file=/mnt/testrun_docker/BAM/1465_1024-pfc-bulk.rehead.bam \
     -P heterozygous_filter.sex=$gender \
     -P output_dir=/mnt/testrun_docker/OUTPUT/$proband_id.parameters.log
@@ -76,7 +75,6 @@ depth=$(grep "average depth" $outdir/$proband_id.parameters.log/stdout*.log | cu
 docker run -it --rm \
   -v "$git_dir:/mnt/mosaic-all" \
   -v "$testrun_dir:/mnt/testrun_docker" \
-  -v "$refgen:/mnt/refgen" \
   -e COMMONERROR="$common_error" \
   -e REPEATS="$repeats" \
   rborgesm/mosaichunter:1.0 \
@@ -84,7 +82,7 @@ docker run -it --rm \
   MHDIR=/usr/MosaicHunter
   java -Xmx4G -jar \$MHDIR/build/mosaichunter.jar \
     -C \$MHDIR/conf/exome.properties \
-    -P reference_file=/mnt/refgen/hs37d5.fa \
+    -P reference_file=/mnt/testrun_docker/Resources/hs37d5.fa \
     -P input_file=/mnt/testrun_docker/BAM/1465_1024-pfc-bulk.rehead.bam \
     -P mosaic_filter.sex=$gender \
     -P depth=$depth \
@@ -121,9 +119,17 @@ wget https://storage.googleapis.com/gatk-best-practices/somatic-b37/af-only-gnom
 # Pull Docker image
 docker image pull broadinstitute/gatk:4.4.0.0 || { echo "Failed to pull Docker image."; exit 1; }
 
+#create dict
+docker run -it --rm \
+  -v "$testrun_dir:/mnt/testrun_docker" \
+  broadinstitute/gatk:4.4.0.0 \
+  /bin/bash -c "
+  gatk CreateSequenceDictionary \
+  -R /mnt/testrun_docker/Resources/hs37d5.fa
+"
+
 # Mutect2.1 Variant calling
 docker run -it --rm \
-  -v "$git:/mnt/mosaic-all" \
   -v "$testrun_dir:/mnt/testrun_docker" \
   broadinstitute/gatk:4.4.0.0 \
   /bin/bash -c "
@@ -140,7 +146,6 @@ docker run -it --rm \
 # Mutect2.2 Tag each variant
 
 docker run -it --rm \
-  -v "$git:/mnt/mosaic-all" \
   -v "$testrun_dir:/mnt/testrun_docker" \
   broadinstitute/gatk:4.4.0.0 \
   /bin/bash -c "
@@ -155,6 +160,125 @@ docker run -it --rm \
 
 # Mutect2.3 Manual Filtration
 bcftools view -O v -f PASS -i '(FORMAT/AD[0:1] >= 5 & FORMAT/DP[0]>=20) && (FORMAT/AF[0:0] <=0.4 || FORMAT/AF[0:0]>=0.7)' "$outdir/$proband_id.Mutect2.$config.singlemode.filtered.vcf" > "$outdir/$proband_id.Mutect2.$config.singlemode.PASS.aaf.vcf" || { echo "Mutect2.3 Manual Filtration failed."; exit 1; }
+
+
+
+#####################################################################   MOSAICFORECAST   ################################################################
+#!/bin/bash
+
+# Define variables
+#git_dir="/home/neuro/Documents/Nandini/MosaiC-All"
+#git_dir=/Users/nandinisandran/Desktop/MosaiC-All/TestRun_docker
+
+# Other variables should not be changed
+#testrun_dir="$git_dir/TestRun_docker"
+#bam_dir="$testrun_dir/BAM"
+#proband_bam_file="$bam_dir/1465_1024-pfc-bulk.rehead.bam"
+#proband_id="1465_1024-pfc-bulk"
+#gender="M"
+#config="hg19"
+#outdir="$testrun_dir/OUTPUT"
+#resources="$testrun_dir/Resources"
+
+#if [ ! -d "${outdir}" ]; then
+#    mkdir -p ${outdir}
+#fi
+
+#if [ ! -d "${resources}" ]; then
+#    mkdir -p ${resources}
+#fi
+
+# download, extract and create index for reference genome
+
+#if [ ! -e "$resources/hs37d5.fa.gz" ]; then
+#    # download
+#    wget "https://storage.googleapis.com/genomics-public-data/references/hs37d5/hs37d5.fa.gz" -P "$resources"
+#    # Extract the tar.gz file
+#    gunzip "$resources/hs37d5.fa.gz"
+#    samtools faidx "$resources/hs37d5.fa"
+#fi
+
+# Ensure Docker image was pulled successfully
+docker image pull yanmei/mosaicforecast:0.0.1 || { echo "Failed to pull MosaicForecast Docker image."; exit 1; }
+
+# download resources for MosaicForecast run
+
+# 1/2 "model_trained" folder from MF which cannot be found in docker
+mkdir $resources/MFgit
+git clone --depth 1 --branch master https://github.com/parklab/MosaicForecast.git --single-branch $resources/MFgit
+
+# 2/2 Umap score (k=24, GRCh37/hg19)
+wget -q https://bismap.hoffmanlab.org/raw/hg19.umap.tar.gz -P "$resources"
+
+# Check if hg19.umap.tar.gz was downloaded successfully
+if [ -e "$resources/hg19.umap.tar.gz" ]; then
+    # Extract the tar.gz file
+    tar -zxvf "$resources/hg19.umap.tar.gz" -C "$resources"
+
+    # Check if extraction was successful
+    if [ $? -eq 0 ]; then
+        echo "Extraction of hg19.umap.tar.gz completed successfully."
+    else
+        echo "Error: Failed to extract hg19.umap.tar.gz."
+        exit 1
+    fi
+fi
+
+# Run the Docker container
+docker run -it --rm \
+  -v "$resources:/mnt/resources" \
+  yanmei/mosaicforecast:0.0.1 \
+  /bin/bash -c '
+    cd "/mnt/resources/hg19"
+    # Fetch chromosome sizes
+    fetchChromSizes hg19 > hg19.chrom.sizes
+    # Decompress the Umap score file
+    zcat /mnt/resources/hg19/k24.umap.wg.gz > /mnt/resources/hg19/k24.umap.wg
+    # Convert wig to BigWig format
+    wigToBigWig /mnt/resources/hg19/k24.umap.wg /mnt/resources/hg19/hg19.chrom.sizes /mnt/resources/hg19/k24.umap.wg.bw
+' || { echo "Processing Umap score for MosaicForecast failed."; exit 1; }
+
+
+# MF1.Prepare Input files
+# Assign BAM prefix
+BAMprefix=$(basename "$proband_bam_file" | sed 's/\.[^.]*$//')
+echo "$BAMprefix"
+
+# Create temporary file
+tmp_file=$(mktemp) || { echo "Failed to create temporary file."; exit 1; }
+
+# Process Mutect2 files
+grep -v "#" "$outdir/$proband_id.Mutect2.$config.PON.gnomad.vcf" | tr " " "\t" > "$tmp_file"
+awk -F '\t' '{print $1, $2-1, $2, $4, $5}' "$tmp_file" > "$outdir/$proband_id.forPhasing.bed"
+awk -v prefix="$BAMprefix" 'BEGIN{OFS="\t"} {$6 = prefix; print}' $outdir/$proband_id.forPhasing.bed > $outdir/$proband_id.MF.$config.phasingInput.bed
+rm "$tmp_file"  # Clean up temporary file
+
+
+echo "$outdir/$proband_id.MF.$config.phasingInput.bed"
+ls -l "$outdir/$proband_id.MF.$config.phasingInput.bed"
+
+
+# MF2.Run Docker command for ReadLevel Features extraction
+docker run -it --rm \
+  -v "$git_dir:/MF" \
+  -v "$testrun_dir:/mnt/testrun_docker" \
+  yanmei/mosaicforecast:0.0.1 \
+  /bin/bash -c " \
+  ReadLevel_Features_extraction.py \
+  /mnt/testrun_docker/OUTPUT/$proband_id.MF.$config.phasingInput.bed \
+  /mnt/testrun_docker/OUTPUT/$proband_id.MF.$config.features.bed \
+  /mnt/testrun_docker/BAM \
+  /mnt/testrun_docker/Resources/hs37d5.fa \
+  /mnt/testrun_docker/Resources/$config/k24.umap.wg.bw \
+  2 \
+  bam \
+  && \
+  Prediction.R \
+  /mnt/testrun_docker/OUTPUT/$proband_id.MF.$config.features.bed \
+  /mnt/testrun_docker/Resources/MFgit/models_trained/50xRFmodel_addRMSK_Refine.rds \
+  Refine \
+  /mnt/testrun_docker/OUTPUT/$proband_id.MF.$config.genotype.predictions.refined.bed \
+" || { echo "Variant calling for MosaicForecast failed."; exit 1; }
 
 
 
